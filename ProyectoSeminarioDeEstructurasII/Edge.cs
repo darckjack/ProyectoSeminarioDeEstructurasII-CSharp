899a26f17a1240f214c6a454bc46fe924e7541af
Juan Gómez <jjgomez@outlook.com>
2015-09-27 21:35:32 -0500

    Added the Node and Edge classes

---
+++ ProyectoSeminarioDeEstructurasII/Edge.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace SeminarioDeEstructurasII {
+    public class Edge<T> {
+
+        Node<T> from;
+        Node<T> to;
+        double edgeWeight;
+        bool edgeIsDirected;
+
+        public Edge(Node<T> fromNode, Node<T> toNode, bool isDirected) : this(fromNode, toNode, 0, isDirected) { }
+
+        public Edge(Node<T> fromNode, Node<T> toNode, double weight, bool isDirected) {
+            if (fromNode == null) {
+                throw new ArgumentNullException("fromNode");
+            }
+
+            if (toNode == null) {
+                throw new ArgumentNullException("toNode");
+            }
+
+            from = fromNode;
+            to = toNode;
+            edgeWeight = weight;
+            edgeIsDirected = isDirected;
+        }
+
+        public Node<T> FromNode {
+            get {
+                return from;
+            }
+        }
+
+        public Node<T> ToNode {
+            get {
+                return to;
+            }
+        }
+
+        public bool IsDirected {
+            get {
+                return edgeIsDirected;
+            }
+        }
+
+        public double Weight {
+            get {
+                return edgeWeight;
+            }
+        }
+
+        public Node<T> GetPartnerNode(Node<T> Node) {
+            if (from == Node) {
+                return to;
+            } else if (to == Node) {
+                return from;
+            } else {
+                throw new ArgumentException("Node");
+            }
+        }
+    }
+}
+
---
+++ ProyectoSeminarioDeEstructurasII/Node.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace SeminarioDeEstructurasII {
+    public class Node<T> {
+        private T nodeData;
+        private List<Edge<T>> incidentEdges;
+        private List<Edge<T>> emanatingEdges;
+
+        public Node(T data) {
+            nodeData = data;
+            incidentEdges = new List<Edge<T>>();
+            emanatingEdges = new List<Edge<T>>();
+        }
+
+        public T Data {
+            get {
+                return nodeData;
+            }
+            set {
+                nodeData = value;
+            }
+        }
+
+        public int IncidentEdgesCount {
+            get {
+                return incidentEdges.Count;
+            }
+        }
+
+        public int Degree {
+            get {
+                return emanatingEdges.Count;
+            }
+        }
+
+        public IEnumerator<Edge<T>> IncidentEdges {
+            get {
+                return incidentEdges.GetEnumerator();
+            }
+        }
+
+        public IEnumerator<Edge<T>> EmanatingEdges {
+            get {
+                return emanatingEdges.GetEnumerator();
+            }
+        }
+
+        public bool HasEmanatingEdgeTo(Node<T> toNode) {
+            for (int i = 0; i < emanatingEdges.Count; i++) {
+                if (emanatingEdges[i].IsDirected) {
+                    if (emanatingEdges[i].ToNode == toNode) {
+                        return true;
+                    }
+                } else {
+                    if ((emanatingEdges[i].ToNode == toNode) ||
+                        ((emanatingEdges[i].FromNode == toNode))) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasIncidentEdgeWith(Node<T> fromNode) {
+            for (int i = 0; i < incidentEdges.Count; i++) {
+                if ((incidentEdges[i].FromNode == fromNode) ||
+                    (incidentEdges[i].ToNode == fromNode)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Edge<T> GetEmanatingEdgeTo(Node<T> toNode) {
+            for (int i = 0; i < emanatingEdges.Count; i++) {
+                if (emanatingEdges[i].IsDirected) {
+                    if (emanatingEdges[i].ToNode == toNode) {
+                        return emanatingEdges[i];
+                    }
+                } else {
+                    if ((emanatingEdges[i].FromNode == toNode) ||
+                        (emanatingEdges[i].ToNode == toNode)) {
+                        return emanatingEdges[i];
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public Edge<T> GetIncidentEdgeWith(Node<T> toNode) {
+            for (int i = 0; i < incidentEdges.Count; i++) {
+                if ((incidentEdges[i].ToNode == toNode) ||
+                    (incidentEdges[i].FromNode == toNode)) {
+                    return incidentEdges[i];
+                }
+            }
+
+            return null;
+        }
+
+        internal List<Edge<T>> IncidentEdgeList {
+            get {
+                return incidentEdges;
+            }
+        }
+
+        internal List<Edge<T>> EmanatingEdgeList {
+            get {
+                return emanatingEdges;
+            }
+        }
+
+        internal void RemoveEdge(Edge<T> edge) {
+
+            Debug.Assert(edge != null);
+
+            RemoveEdgeFromNode(edge);
+        }
+
+        internal void AddEdge(Edge<T> edge) {
+
+            Debug.Assert(edge != null);
+
+            if (edge.IsDirected) {
+                if (edge.FromNode == this) {
+                    emanatingEdges.Add(edge);
+                }
+            } else {
+                emanatingEdges.Add(edge);
+            }
+
+            incidentEdges.Add(edge);
+        }
+
+        void RemoveEdgeFromNode(Edge<T> edge) {
+
+            Debug.Assert(incidentEdges.Remove(edge),
+                "Edge not found on Node in RemoveEdgeFromNode.");
+
+            incidentEdges.Remove(edge);
+
+            if (edge.IsDirected) {
+                if (edge.FromNode == this) {
+                    emanatingEdges.Remove(edge);
+                }
+            } else {
+                emanatingEdges.Remove(edge);
+            }
+        }
+    }
+}
+
